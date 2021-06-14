fe6e8dba9322dcbdb8d74c8258366c88aae5cbae
Tara Dia <taracdia@gmail.com>
2021-06-14 06:53:39 -0500

    Madlibs

---
+++ madlibs.rb
@@ -0,0 +1,10 @@
+puts "Name a color: "
+color = gets.chomp
+puts "Name a plural noun: "
+plural_noun = gets.chomp
+puts "Name a celebrity: "
+celebrity = gets.chomp
+
+puts "Roses are #{color}"
+puts "#{plural_noun} are blue"
+puts "I love #{celebrity}"
