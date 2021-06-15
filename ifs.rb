819539716a7e5abaaea040801bc0ec9faad8af10
Tara Dia <taracdia@gmail.com>
2021-06-15 14:35:56 -0500

    Coparators

--- ifs.rb
+++ ifs.rb
@@ -11,3 +11,15 @@ else
   puts "You are  not male and not tall"
 
 end
+
+def max(num1, num2, num3)
+  if num1 >= num2 and num1 >=num3
+    return num1
+  elsif num2 >= num1 and num2 >= num3
+    return num2
+  else
+    return num3
+  end
+end
+
+puts max(1,3,2)
