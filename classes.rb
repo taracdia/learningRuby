245681e219b45df8aee54da9cd26d930869f9035
Tara Dia <taracdia@gmail.com>
2021-06-15 18:26:29 -0500

    Classes

---
+++ classes.rb
@@ -0,0 +1,12 @@
+class Book
+  attr_accessor :title, :author, :pages
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
+  end
+end
+
+book1 = Book.new("title", "author", 200)
+
+puts book1
