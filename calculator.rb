d3406ff9f174101cbf964d3be324ea7bb2029ffe
Tara Dia <taracdia@gmail.com>
2021-06-15 15:23:37 -0500

    Upgraded calculator

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,21 @@
 puts "Enter a number: "
 num1 = gets.chomp.to_f
+puts "Enter operator: "
+op = gets.chomp
 puts "Enter another number: "
 num2 = gets.chomp.to_f
-puts (num1 + num2)
+
+if op == "+"
+  puts (num1 + num2)
+elsif op == "-"
+  puts (num1 - num2)
+
+elsif op == "*"
+  puts (num1 * num2)
+
+elsif op == "/"
+  puts (num1 / num2)
+
+else
+  put "Error"
+end
