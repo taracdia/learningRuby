e06ebd9e18eb6f9634d4354832eae0dd0b053ea1
Tara Dia <taracdia@gmail.com>
2021-06-15 14:32:49 -0500

    Simple if

---
+++ ifs.rb
@@ -0,0 +1,13 @@
+ismale = true
+istall = false
+
+if ismale and istall
+  puts "You are a tall male"
+elsif ismale and !istall
+  puts "You are a short male"
+elsif !ismale and istall
+  puts "You are not male but are tall"
+else
+  puts "You are  not male and not tall"
+
+end
